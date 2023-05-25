a5c8a8bf0bf603103c6a81d1b73c003673ea4697
MilindAmrutkar <milind27.amrutkar@gmail.com>
2023-05-25 19:44:33 +0530

    Created Lazy Columns - scrollable list of movies

--- app/src/main/java/com/backtocding/moviebasicapp/MainActivity.kt
+++ app/src/main/java/com/backtocding/moviebasicapp/MainActivity.kt
@@ -4,14 +4,24 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -21,8 +31,9 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -33,7 +44,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
-                MainContent(it)
+                MainContent(modifier = Modifier.padding(top = it.calculateTopPadding()))
             }
         }
     }
@@ -72,7 +83,18 @@ fun MyApp(content: @Composable (PaddingValues) -> Unit) {
 }
 
 @Composable
-fun MainContent(paddingValues: PaddingValues) {
+fun MainContent(
+    modifier: Modifier,
+    moviesList: List<String> = listOf(
+        "Avatar",
+        "300",
+        "Harry Potter",
+        "Happiness",
+        "Life of Pie",
+        "Kal Ho Na Ho",
+        "Zindagi Na Milego Dobara"
+    )
+) {
 //    Surface(
 //        modifier = Modifier.padding(top = paddingValues.calculateTopPadding()),
 //        color = MaterialTheme.colorScheme.background
@@ -80,14 +102,51 @@ fun MainContent(paddingValues: PaddingValues) {
 
 //    }
 
-    Text(text = "Hello")
+    Column(
+        modifier = modifier.padding(
+            12.dp
+        )
+    ) {
+        LazyColumn {
+            items(items = moviesList) {
+                MovieRow(movie = it)
+            }
+        }
+    }
 }
 
+@Composable
+fun MovieRow(movie: String) {
+    Card(
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth()
+            .height(130.dp),
+        shape = RoundedCornerShape(corner = CornerSize(16.dp)),
+        elevation = CardDefaults.cardElevation(6.dp)
+    ) {
+
+        Row(verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Start) {
+            Surface(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .size(100.dp),
+                shape = RectangleShape,
+                shadowElevation = 4.dp
+            ) {
+                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Movie Image")
+
+            }
+            Text(text = movie)
+        }
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MyApp {
-        MainContent(it)
+        MainContent(modifier = Modifier.padding(top = it.calculateTopPadding()))
     }
 }
