dccb99a9f111e9655b4922ff27799a13aae2479d
MilindAmrutkar <milind27.amrutkar@gmail.com>
2023-05-25 22:41:25 +0530

    Added: Coil dependency to load images

--- app/src/main/java/com/backtocding/moviebasicapp/widgets/Widgets.kt
+++ app/src/main/java/com/backtocding/moviebasicapp/widgets/Widgets.kt
@@ -1,7 +1,9 @@
 package com.backtocding.moviebasicapp.widgets
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -9,22 +11,28 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
+import coil.transform.CircleCropTransformation
 import com.backtocding.moviebasicapp.model.Movie
+import com.backtocding.moviebasicapp.model.getMovies
 
+@Preview
 @Composable
-fun MovieRow(movie: Movie, onItemClick: (String) -> Unit) {
+fun MovieRow(
+    movie: Movie = getMovies()[0],
+    onItemClick: (String) -> Unit = {}
+) {
     Card(
         modifier = Modifier
             .padding(4.dp)
@@ -46,10 +54,22 @@ fun MovieRow(movie: Movie, onItemClick: (String) -> Unit) {
                 shape = RectangleShape,
                 shadowElevation = 4.dp
             ) {
-                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Movie Image")
-
+                Image(
+                    painter = rememberImagePainter(
+                        data = movie.poster,
+                        builder = {
+                            crossfade(true)
+                            transformations(CircleCropTransformation())
+                        }
+                    ),
+                    contentDescription = "Movie Poster"
+                )
             }
-            Text(text = movie.title)
+            Column(modifier = Modifier.padding(4.dp)) {
+                Text(text = movie.title, style = MaterialTheme.typography.titleLarge)
+                Text(text = "Director: ${movie.title}", style = MaterialTheme.typography.titleSmall)
+                Text(text = "Released: ${movie.year}", style = MaterialTheme.typography.titleSmall)
+            }
         }
     }
 }
