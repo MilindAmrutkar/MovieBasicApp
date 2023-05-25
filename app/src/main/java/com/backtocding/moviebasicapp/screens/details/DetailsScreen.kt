f7d4dad543ca820f79e0c7e1a42886337a786a48
MilindAmrutkar <milind27.amrutkar@gmail.com>
2023-05-25 21:45:59 +0530

    Added: TopAppBar to DetailsScreen

--- app/src/main/java/com/backtocding/moviebasicapp/screens/details/DetailsScreen.kt
+++ app/src/main/java/com/backtocding/moviebasicapp/screens/details/DetailsScreen.kt
@@ -1,39 +1,65 @@
 package com.backtocding.moviebasicapp.screens.details
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.material3.Button
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DetailsScreen(navController: NavController, movieDate: String?) {
-    Surface(modifier = Modifier
-        .fillMaxHeight()
-        .fillMaxWidth()) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
+    Scaffold(topBar = {
+        TopAppBar(
+            scrollBehavior = scrollBehavior,
+            navigationIcon = {
+                IconButton(onClick = {
+                    navController.popBackStack()
+                }) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack,
+                        contentDescription = "Arrow Back"
+                    )
+                }
+            },
+            title = {
+                Text(text = "Movie Details")
+            },
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = MaterialTheme.colorScheme.surfaceColorAtElevation(5.dp)
+            )
+        )
+    }) {
+        Surface(
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth()
         ) {
-            Text(text = movieDate.toString(), style = MaterialTheme.typography.headlineSmall)
-
-            Spacer(modifier = Modifier.height(23.dp))
-            Button(onClick = {
-                navController.popBackStack()
-            }) {
-                Text(text = "Go Back")
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(text = movieDate.toString(), style = MaterialTheme.typography.headlineSmall)
             }
-
         }
     }
 }
