0cde739c58c338ae032ee548c4045004534cadda
MilindAmrutkar <milind27.amrutkar@gmail.com>
2023-05-25 23:21:55 +0530

    Added: HorizontalScrollableImageView

--- app/src/main/java/com/backtocding/moviebasicapp/model/Movie.kt
+++ app/src/main/java/com/backtocding/moviebasicapp/model/Movie.kt
@@ -22,9 +22,14 @@ fun getMovies(): List<Movie> {
             genre = "Action, Adventure, Fantasy",
             director = "James Cameron",
             actors = "Sam ",
-            plot = "abjalsdfjkla ew asdjfla lsdjf lsf",
+            plot = "On the lush alien world of Pandora live the Na'vi, beings who appear primitive but are highly evolved. Because the planet's environment is poisonous, human/Na'vi hybrids, called Avatars, must link to human minds to allow for free movement on Pandora. Jake Sully (Sam Worthington), a paralyzed former Marine, becomes mobile again through one such Avatar and falls in love with a Na'vi woman (Zoe Saldana). As a bond with her grows, he is drawn into a battle for the survival of her world.",
             poster = "https://lumiere-a.akamaihd.net/v1/images/avatar_800x1200_208c9665.jpeg?region=0%2C0%2C800%2C1200",
-            images = listOf("https://www.shutterstock.com/image-vector/online-cinema-art-movie-watching-260nw-584655766.jpg", "https://lumiere-a.akamaihd.net/v1/images/avatar_800x1200_208c9665.jpeg?region=0%2C0%2C800%2C1200"),
+            images = listOf(
+                "https://www.google.com/url?sa=i&url=https%3A%2F%2Fsixactstructure.com%2Fstory-structure-analysis-avatar%2F&psig=AOvVaw1SNYLv1CsX1HYI7UsmUM_t&ust=1685123132863000&source=images&cd=vfe&ved=2ahUKEwjvopX_gpH_AhW0bmwGHTJ3D1gQjRx6BAgAEAw",
+                "https://images-na.ssl-images-amazon.com/images/I/51wYAC4gG3L.jpg",
+                "https://www.google.com/url?sa=i&url=https%3A%2F%2Fthemoviespoiler.com%2Fmovies%2Favatar%2F&psig=AOvVaw3VAKaMQmaN2wSSdPdPxQ0L&ust=1685123134022000&source=images&cd=vfe&ved=2ahUKEwiPh9z_gpH_AhVTa2wGHYABCTEQjRx6BAgAEAw",
+                "https://static1.moviewebimages.com/wordpress/wp-content/uploads/2022/09/Avatar-movie-from-James-Cameron.jpg"
+            ),
             rating = "7.9"
         ),
         Movie(
@@ -32,11 +37,16 @@ fun getMovies(): List<Movie> {
             title = "Kal Ho Na Ho",
             year = "2004",
             genre = "Action, Adventure, Fantasy",
-            director = "James Cameron",
-            actors = "Sam ",
-            plot = "abjalsdfjkla ew asdjfla lsdjf lsf",
-            poster = "https://lumiere-a.akamaihd.net/v1/images/avatar_800x1200_208c9665.jpeg?region=0%2C0%2C800%2C1200",
-            images = listOf("https://www.shutterstock.com/image-vector/online-cinema-art-movie-watching-260nw-584655766.jpg", "https://lumiere-a.akamaihd.net/v1/images/avatar_800x1200_208c9665.jpeg?region=0%2C0%2C800%2C1200"),
+            director = "Karan Johar",
+            actors = "Shahrukh Khan",
+            plot = " Naina Catherine Kapur (Preity Zinta) is an angry young girl, for more than one reason. Her father committed suicide when she needed him the most, leaving her mother Jennifer (Jaya Bachchan) to raise their two young children Shiv (Atith Naik) & Gia (Jhanak Shukla) all alone.",
+            poster = "https://upload.wikimedia.org/wikipedia/en/4/45/Kal_Ho_Naa_Ho.jpg",
+            images = listOf(
+                "https://upload.wikimedia.org/wikipedia/en/4/45/Kal_Ho_Naa_Ho.jpg",
+                "https://m.media-amazon.com/images/M/MV5BYmVjNDIxODAtNWZiZi00ZDBlLWJmOTUtNDNjMGExNTViMzE1XkEyXkFqcGdeQXVyNTE0MDc0NTM@._V1_FMjpg_UX1000_.jpg",
+                "https://i.ytimg.com/vi/tVMAQAsjsOU/maxresdefault.jpg",
+                "https://stat4.bollywoodhungama.in/wp-content/uploads/2016/03/Kal-Ho-Naa-Ho-306x393.jpg"
+            ),
             rating = "7.9"
         )
     )
--- app/src/main/java/com/backtocding/moviebasicapp/screens/details/DetailsScreen.kt
+++ app/src/main/java/com/backtocding/moviebasicapp/screens/details/DetailsScreen.kt
@@ -1,13 +1,22 @@
 package com.backtocding.moviebasicapp.screens.details
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -23,6 +32,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.rememberImagePainter
+import com.backtocding.moviebasicapp.model.Movie
 import com.backtocding.moviebasicapp.model.getMovies
 import com.backtocding.moviebasicapp.widgets.MovieRow
 
@@ -68,7 +79,29 @@ fun DetailsScreen(navController: NavController, movieId: String?) {
                 verticalArrangement = Arrangement.Top
             ) {
                 MovieRow(movie = newMovieList.first())
-                Text(text = newMovieList.first().title, style = MaterialTheme.typography.headlineSmall)
+                Spacer(modifier = Modifier.height(8.dp))
+                Divider()
+                Text(text = "Movie Images")
+                HorizontalScrollableImageView(newMovieList)
+            }
+        }
+    }
+}
+
+@Composable
+private fun HorizontalScrollableImageView(newMovieList: List<Movie>) {
+    LazyRow {
+        items(newMovieList[0].images) { image ->
+            Card(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .size(240.dp),
+                elevation = CardDefaults.cardElevation(5.dp)
+            ) {
+                Image(
+                    painter = rememberImagePainter(data = image),
+                    contentDescription = "Movie Poster"
+                )
             }
         }
     }
